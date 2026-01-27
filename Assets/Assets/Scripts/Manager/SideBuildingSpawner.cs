a8567957fb77117749dc502697afdd6ca3c0bae2
chain3132 <148683922+chain3132@users.noreply.github.com>
2026-01-27 21:19:22 +0700

    Pretest final

--- Assets/Assets/Scripts/Manager/SideBuildingSpawner.cs
+++ Assets/Assets/Scripts/Manager/SideBuildingSpawner.cs
@@ -5,19 +5,35 @@ public class SideBuildingSpawner : MonoBehaviour
 {
     [Header("Building Prefabs")]
     public GameObject[] buildingPrefabs;
+    public GameObject[] treesPrefabs;
 
       [Header("Roots")]
     public Transform leftRoot;
     public Transform rightRoot;
+    
+    public Transform treesLeftRoot;
+    public Transform treesRightRoot;
 
     [Header("Z Range")]
     public float startZ = 0f;
     public float endZ = 120f;
     public float spacing = 10f;
+    [Header("Tree Random Spacing")]
+    public float treeMinSpacing = 8f;
+    public float treeMaxSpacing = 20f;
 
     [Header("X Position")]
     public float leftX = -12f;
     public float rightX = 12f;
+    
+    [Header("Tree X Range")]
+    public float treesLeftMinX = -20f;
+    public float treesLeftMaxX = -10f;
+
+    public float treesRightMinX = 10f;
+    public float treesRightMaxX = 20f;
+    
+    
 
     [Header("Movement")]
     public float recycleZ = -2f;
@@ -25,16 +41,86 @@ public class SideBuildingSpawner : MonoBehaviour
     List<GameObject> leftPool = new List<GameObject>();
     List<GameObject> rightPool = new List<GameObject>();
 
+    List<GameObject> treesLeftPool = new List<GameObject>();
+    List<GameObject> treesRightPool = new List<GameObject>();
+
     void Start()
     {
         InitSide(true);
         InitSide(false);
+        
+        InitTrees(true);
+        InitTrees(false);
     }
 
     void Update()
     {
         UpdateSide(leftPool, true);
         UpdateSide(rightPool, false);
+        
+        
+        UpdateTrees(treesLeftPool, true);
+        UpdateTrees(treesRightPool, false);
+    }
+    GameObject CreateTreeHolder(float x, float z, Transform parent)
+    {
+        GameObject holder = new GameObject("TreeHolder");
+        holder.transform.SetParent(parent);
+        holder.transform.position = new Vector3(x, 1, z);
+
+        GameObject tree = Instantiate(
+            treesPrefabs[Random.Range(0, treesPrefabs.Length)],
+            holder.transform
+        );
+
+        AlignToGround(tree);
+
+        var mover = holder.AddComponent<MovingBuilding>();
+        mover.speed = 13f;
+
+        return holder;
+    }
+    void UpdateTrees(List<GameObject> pool, bool isLeft)
+    {
+        GameObject first = pool[0];
+
+        if (first.transform.position.z < recycleZ)
+        {
+            GameObject last = pool[pool.Count - 1];
+
+            float minX = isLeft ? treesLeftMinX : treesRightMinX;
+            float maxX = isLeft ? treesLeftMaxX : treesRightMaxX;
+
+            float newX = Random.Range(minX, maxX);
+            float randomSpacing = Random.Range(treeMinSpacing, treeMaxSpacing);
+            float newZ = last.transform.position.z + randomSpacing;
+
+            first.transform.position = new Vector3(newX, 0f, newZ);
+
+            pool.RemoveAt(0);
+            pool.Add(first);
+        }
+    }
+    void InitTrees(bool isLeft)
+    {
+        Transform root = isLeft ? treesLeftRoot : treesRightRoot;
+        List<GameObject> pool = isLeft ? treesLeftPool : treesRightPool;
+
+        float minX = isLeft ? treesLeftMinX : treesRightMinX;
+        float maxX = isLeft ? treesLeftMaxX : treesRightMaxX;
+
+        float z = startZ;
+
+        while (z <= endZ)
+        {
+            float x = Random.Range(minX, maxX);
+
+            GameObject holder = CreateTreeHolder(x, z, root);
+            pool.Add(holder);
+
+            // 🔀 random spacing
+            z += Random.Range(treeMinSpacing, treeMaxSpacing);
+        }
     }
 
     void InitSide(bool isLeft)
